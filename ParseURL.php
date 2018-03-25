8c64fa3d4c4faf46c8ed35a8c8bad54f93d20d5f
Alexey <ALexdestv97@gmail.com>
2018-03-26 01:32:46 +0300

    initial #2

--- ParseURL.php
+++ ParseURL.php
@@ -13,9 +13,13 @@ $pq = phpQuery::newDocument($content);
 
 foreach($keywords as $key)
 {
+  echo(str_replace($key,'<b style="color:yellow; background-color:blue">Папавсь</b>',$content));
+
+  /*
   echo('<div class="w3-panel w3-light-gray w3-leftbar w3-border-blue-gray">');
   echo('<h3>'.$key.'</h3>');
   $pq->find(":contains('$key'):has(:not(:contains('$key'))")->append('<b style="color:yellow; background-color:blue">Папавсь</b>');
   echo($pq->html());
   echo('</div>');
+  */
 }
