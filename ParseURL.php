894d1391f428d303dc17166e26dde0b8f7729667
Alexey <ALexdestv97@gmail.com>
2018-03-26 23:22:40 +0300

    Result to iframe

--- ParseURL.php
+++ ParseURL.php
@@ -9,13 +9,9 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_URL, $url);
 $content = curl_exec ($ch);
 
-$pq = phpQuery::newDocument($content);
-
 foreach($keywords as $key)
 {
-  echo('<div class="w3-panel w3-light-gray w3-leftbar w3-border-blue-gray">');
-  echo('<h3>'.$key.'</h3>');
-  $pq->find(":contains('$key'):has(:not(:contains('$key'))")->append('<b style="color:yellow; background-color:blue">Папавсь</b>');
+  $pq = phpQuery::newDocument(str_replace($key, "<b style='color:yellow; background-color:blue'>$key</b>", $content));
+  $pq->find("script, link")->remove();
   echo($pq->html());
-  echo('</div>');
 }
--- parseQuery.js
+++ parseQuery.js
@@ -1,9 +1,3 @@
-function AppendResult(inner)
-{
-	var element='<div class="w3-panel w3-light-gray w3-leftbar w3-border-blue-gray">';
-	element=element.concat(inner,'</div>');
-	$("#result").append(element);
-}
 
 function ParseQuery()
 {
@@ -18,19 +12,10 @@ function ParseQuery()
     });
     if(keywords)
     {
+    
     keywords = keywords.slice(0, -1);
     $query="ParseUrl.php?url="+$("#urlInput").val()+"&keys="+keywords;
-    
-    /*$("#result").empty();
-	$("#parseLoader").show();
-    $.get($query, function(data, status)
-	{
-		$("#parseLoader").hide();
-		$("#result").append(data);
-    });
-	*/
-
-	//window.location.replace($query);
-	window.location.href = $query;
+   	$("#result").attr('src',$query);
+	
 	}
 }
