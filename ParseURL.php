fe6bf73da9d5bef5dad93f329cc7ba9a13c6a812
DESKTOP-C9DKVHH\fortrou <fortrou@gmail.com>
2018-03-26 23:52:28 +0300

    переход на навигацию в фрейме

--- ParseURL.php
+++ ParseURL.php
@@ -8,10 +8,17 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_URL, $url);
 $content = curl_exec ($ch);
-
+$iter = 0;
+$new_array = array();
+$string_navigation = '<ul style="position: fixed; width:200px; background: gray; top: 0; left: 0;">';
 foreach($keywords as $key)
 {
-  $pq = phpQuery::newDocument(str_replace($key, "<b style='color:yellow; background-color:blue'>$key</b>", $content));
-  $pq->find("script, link")->remove();
-  echo($pq->html());
+  $string_navigation .= sprintf('<li><a href="#">%s</a></li>', $key);
+  $new_array[] = "<b class='popavs' data-key='$key' style='color:yellow; background-color:blue'>$key</b>";
+
 }
+$string_navigation .= "</ul>";
+$content = str_replace($keywords, $new_array, $content) . $string_navigation;
+$pq = phpQuery::newDocument($content);
+$pq->find("script, link")->remove();
+echo($pq->html());
